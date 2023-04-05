c7384e5b17b0e43a6939a07f95c2ada9f0e46d0c
Dinesh Chukkala <dineshchukkala169@gmail.com>
2023-04-05 14:32:42 +0530

    Cloned the template of solid js

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+    </div>
+  );
+};
+
+export default App;
